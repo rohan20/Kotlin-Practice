00a5eb06488204a1f6d6e4a0acbe0818dd9841b7
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-10 21:42:52 +0530

    using when()

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -4,7 +4,18 @@ Created by rohan on 10/11/17
 
 fun main(args: Array<String>){
     println()
-    println("What's your name?")
-    val name = readLine();
-    println("Hello " + name)
+//    println("What's your name?")
+//    val name = readLine();
+//    println("Hello " + name)
+
+//    val number = 2
+//    val answer = number + 5
+    val newValue: Int? = readLine()?.toInt()
+    println(newValue)
+
+    when(newValue){
+        2 -> println("Two")
+        5 -> println("Five")
+    }
+
 }
