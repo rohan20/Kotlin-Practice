37607aa27a69dbc0709540866fdf10dc4bd5b9cb
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-12 02:55:59 +0530

    arrayListOf<>()

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -5,9 +5,14 @@ Created by rohan on 10/11/17
 fun main(args: Array<String>){
     println()
 
+    //list is read only (no add() method)
     val companiesList = listOf<String>("Google", "Apple", "Samsung", "OnePlus")
+    //to add elements, use arrayListOf<>()
+    val newCompaniesList = arrayListOf<String>()
+    newCompaniesList.add("Nokia")
+    newCompaniesList.addAll(companiesList)
 
-    for(company in companiesList){
+    for(company in newCompaniesList){
         println()
         print("Hello, ")
         print(company)
