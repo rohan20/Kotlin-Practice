df207b4f54c0931e16177ade7d19d5c5834d63a8
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-11 22:10:07 +0530

    lists in kotlin

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -4,18 +4,7 @@ Created by rohan on 10/11/17
 
 fun main(args: Array<String>){
     println()
-//    println("What's your name?")
-//    val name = readLine();
-//    println("Hello " + name)
 
-//    val number = 2
-//    val answer = number + 5
-    val newValue: Int? = readLine()?.toInt()
-    println(newValue)
-
-    when(newValue){
-        2 -> println("Two")
-        5 -> println("Five")
-    }
+    val companiesList = listOf<String>("Google", "Apple", "Samsung", "OnePlus")
 
 }
