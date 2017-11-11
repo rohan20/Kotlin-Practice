5dd567606a93bf8af71589c9bfae1f10d1934bb2
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-11 22:14:12 +0530

    testing list methods

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -6,5 +6,9 @@ fun main(args: Array<String>){
     println()
 
     val companiesList = listOf<String>("Google", "Apple", "Samsung", "OnePlus")
+    println("print list: " + companiesList)
+    println("print reverse list: " + companiesList.asReversed())
+    println("check if list is empty: " + companiesList.isEmpty())
+
 
 }
