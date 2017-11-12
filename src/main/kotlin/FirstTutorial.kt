7f0bbeeae6c77a3f9e8695f34ee4032d2681af6d
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-13 03:57:09 +0530

    delete from arrayList

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -6,21 +6,44 @@ fun main(args: Array<String>) {
     println()
 
     val newCompaniesList = arrayListOf<String>()
-//    newCompaniesList.add("Nokia")
+    newCompaniesList.add("Nokia")
 
-    var newCompanyName: String = "dummyName"
+    var newCompanyName: String
+    var companyNameToBeDeleted: String
+    var userChoice = "dummyInput"
 
-    //iterate through loop unless user presses the return key i.e. no input
-    while (newCompanyName != "") {
 
-        println()
-        print("Enter new company name: ")
-        newCompanyName = readLine()!!
+    //1. Add company
+    //2. Delete company
+    //3. Exit
+    while (userChoice != "3") {
 
-        newCompaniesList.add(newCompanyName)
+        println("1. Add company")
+        println("2. Delete company")
+        println("3. Exit")
+        userChoice = readLine()!!
+
+        //add
+        if (userChoice == "1") {
+            println()
+            print("Enter new company name: ")
+            newCompanyName = readLine()!!
+            newCompaniesList.add(newCompanyName)
+        } else if (userChoice == "2") {
+            print("Enter company name to be deleted: ")
+            companyNameToBeDeleted = readLine()!!
+
+            newCompaniesList.remove(companyNameToBeDeleted)
+        } else {
+            return
+        }
+
         for (companyName in newCompaniesList) {
             println(companyName)
         }
+
+        println()
+
     }
 
 }
