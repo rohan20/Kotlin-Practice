6158f82641f65a478658ccf7365c44d38aebb23a
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-13 11:43:42 +0530

    passing values to functions

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -4,10 +4,21 @@ Created by rohan on 10/11/17
 
 fun main(args: Array<String>) {
     println()
-    displayGreeting()
-    println("and a Happy New Year!")
+    print("Enter your name: ")
+    val name: String? = readLine()
+    printUsername(name)
+    displayChristmasGreeting()
+    displayNewYearGreeting()
 }
 
-fun displayGreeting(){
-    println("Merry Christmas")
+fun printUsername(name: String?){
+    print("\nHey $name, ")
+}
+
+fun displayChristmasGreeting(){
+    print("we wish you a Merry Christmas")
+}
+
+fun displayNewYearGreeting() {
+    println(" and a Happy New Year!")
 }
