c0ccb2440f4ba9de1ee51e0012b27490ce526938
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-13 11:29:54 +0530

    functions

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -4,46 +4,10 @@ Created by rohan on 10/11/17
 
 fun main(args: Array<String>) {
     println()
+    displayGreeting()
+    println("and a Happy New Year!")
+}
 
-    val newCompaniesList = arrayListOf<String>()
-    newCompaniesList.add("Nokia")
-
-    var newCompanyName: String
-    var companyNameToBeDeleted: String
-    var userChoice = "dummyInput"
-
-
-    //1. Add company
-    //2. Delete company
-    //3. Exit
-    while (userChoice != "3") {
-
-        println("1. Add company")
-        println("2. Delete company")
-        println("3. Exit")
-        userChoice = readLine()!!
-
-        //add
-        if (userChoice == "1") {
-            println()
-            print("Enter new company name: ")
-            newCompanyName = readLine()!!
-            newCompaniesList.add(newCompanyName)
-        } else if (userChoice == "2") {
-            print("Enter company name to be deleted: ")
-            companyNameToBeDeleted = readLine()!!
-
-            newCompaniesList.remove(companyNameToBeDeleted)
-        } else {
-            return
-        }
-
-        for (companyName in newCompaniesList) {
-            println(companyName)
-        }
-
-        println()
-
-    }
-
+fun displayGreeting(){
+    println("Merry Christmas")
 }
