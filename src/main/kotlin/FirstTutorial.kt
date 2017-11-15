d110ba42a76e18e109d92470e9860624a2ffac3e
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-15 15:28:24 +0530

    model class

--- src/main/kotlin/FirstTutorial.kt
+++ src/main/kotlin/FirstTutorial.kt
@@ -1,32 +1,14 @@
+import model.Company
+
 /*
 Created by rohan on 10/11/17  
 */
 
 fun main(args: Array<String>) {
-    println()
-    print("Enter your name: ")
-    val name: String? = readLine()
-    printUsername(name)
-    val christmasGreeting = displayChristmasGreeting()
-    print(christmasGreeting)
-    displayNewYearGreeting()
 
-    val addition = doAdditionOfTwoNumbers(41, 5)
-    println("Addition: $addition")
-}
+    val company = Company()
 
-fun doAdditionOfTwoNumbers(num1: Int, num2: Int): Int {
-    return num1 + num2
-}
+    println("Company Name: " + company.companyName)
+    println("Company Name from getter: " + company.getCarName())
 
-fun printUsername(name: String?) {
-    print("\nHey $name, ")
-}
-
-fun displayChristmasGreeting(): String {
-    return "we wish you a Merry Xmas"
-}
-
-fun displayNewYearGreeting() {
-    println(" and a Happy New Year!")
 }
---
+++ src/main/kotlin/model/Company.kt
@@ -0,0 +1,16 @@
+package model
+
+/*
+Created by rohan on 15/11/17  
+*/
+
+class Company {
+
+    var companyName = "Google";
+
+    fun getCarName(): String {
+        return "Apple"
+    }
+
+}
+
